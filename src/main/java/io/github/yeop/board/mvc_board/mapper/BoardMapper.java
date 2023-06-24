77bb4e2550934d72ed3fc24d5f73a24f8ad8c883
sangyeop <yeop1.dev@gmail.com>
2023-06-25 03:43:26 +0900

    :recycle: refactor(BoardMapper): throw exception 남발 금지

--- src/main/java/io/github/yeop/board/mvc_board/mapper/BoardMapper.java
+++ src/main/java/io/github/yeop/board/mvc_board/mapper/BoardMapper.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    List<BoardDto> selectBoardList() throws Exception;
+    List<BoardDto> selectBoardList();
 
-    void insertBoard(BoardDto board) throws Exception;
+    void insertBoard(BoardDto board);
 
-    void updateHitCount(int boardIdx) throws Exception;
+    void updateHitCount(int id);
 
-    BoardDto selectBoardDetail(int boardIdx) throws Exception;
+    BoardDto selectBoardDetail(int id);
 
-    void updateBoard(BoardDto boardDto) throws Exception;
+    void updateBoard(BoardDto board);
 
-    void deleteBoard(int boardIdx) throws Exception;
+    void deleteBoard(int id);
 }
