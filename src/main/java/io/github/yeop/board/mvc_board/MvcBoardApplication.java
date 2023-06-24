40dd1a39187902e332994b2b61b368d0bfbd53ac
sangyeop <yeop1.dev@gmail.com>
2023-06-25 03:45:15 +0900

    :recycle: refactor(MvcBoardApplication): HiddenHttpMethodFilter설정

--- src/main/java/io/github/yeop/board/mvc_board/MvcBoardApplication.java
+++ src/main/java/io/github/yeop/board/mvc_board/MvcBoardApplication.java
@@ -2,6 +2,8 @@ package io.github.yeop.board.mvc_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MvcBoardApplication {
@@ -9,4 +11,11 @@ public class MvcBoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvcBoardApplication.class, args);
 	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+	//실행할때 자스피티 여기에 넣기
 }
