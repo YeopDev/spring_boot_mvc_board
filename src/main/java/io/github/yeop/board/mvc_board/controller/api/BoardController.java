78f6b2bbc439d4cc1fc598dc5a76829d8e31305c
sangyeop <yeop1.dev@gmail.com>
2023-06-27 02:34:17 +0900

    :recycle: refactor(BoardController): 컨트롤러안에 DTO 수정

--- src/main/java/io/github/yeop/board/mvc_board/controller/api/BoardController.java
+++ src/main/java/io/github/yeop/board/mvc_board/controller/api/BoardController.java
@@ -1,13 +1,12 @@
 package io.github.yeop.board.mvc_board.controller.api;
 
-import io.github.yeop.board.mvc_board.dto.BoardDto;
+import io.github.yeop.board.mvc_board.dto.InsertBoardDto;
+import io.github.yeop.board.mvc_board.dto.UpdateBoardDto;
 import io.github.yeop.board.mvc_board.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/yeop")
@@ -15,17 +14,15 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/board")
-    public ResponseEntity<String> insertBoard(@RequestBody BoardDto board) {
+    public ResponseEntity<String> insertBoard(@RequestBody InsertBoardDto board) {
         boardService.insertBoard(board);
-        log.debug("insert Board");
 
         return ResponseEntity.ok("insert success");
     }
 
     @PutMapping(value = "/board")
-    public ResponseEntity<String> updateBoard(@RequestBody BoardDto board) {
+    public ResponseEntity<String> updateBoard(@RequestBody UpdateBoardDto board) {
         boardService.updateBoard(board);
-        log.debug("update Board");
 
         return ResponseEntity.ok("update success");
     }
@@ -33,7 +30,6 @@ public class BoardController {
     @DeleteMapping("/board/{id}")
     public ResponseEntity<String> deleteBoard(@PathVariable int id) {
         boardService.deleteBoard(id);
-        log.debug("delete Board");
 
         return ResponseEntity.ok("delete success");
     }
