81cffa11f98a78125e132cd7c27ba9f62e3a271a
sangyeop <yeop1.dev@gmail.com>
2023-06-27 02:37:17 +0900

    :recycle: refactor(BoardMapper): DTO수정

--- src/main/java/io/github/yeop/board/mvc_board/mapper/BoardMapper.java
+++ src/main/java/io/github/yeop/board/mvc_board/mapper/BoardMapper.java
@@ -1,21 +1,24 @@
 package io.github.yeop.board.mvc_board.mapper;
 
-import io.github.yeop.board.mvc_board.dto.BoardDto;
+import io.github.yeop.board.mvc_board.dto.BoardDetailDto;
+import io.github.yeop.board.mvc_board.dto.BoardListDto;
+import io.github.yeop.board.mvc_board.dto.InsertBoardDto;
+import io.github.yeop.board.mvc_board.dto.UpdateBoardDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    List<BoardDto> selectBoardList();
+    List<BoardListDto> selectBoardList();
 
-    void insertBoard(BoardDto board);
+    void insertBoard(InsertBoardDto board);
 
     void updateHitCount(int id);
 
-    BoardDto selectBoardDetail(int id);
+    BoardDetailDto selectBoardDetail(int id);
 
-    void updateBoard(BoardDto board);
+    void updateBoard(UpdateBoardDto board);
 
     void deleteBoard(int id);
 }
