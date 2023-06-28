2bde201262de39669ae2a97fdf9d8fe3ae418201
sangyeop <yeop1.dev@gmail.com>
2023-06-29 00:19:52 +0900

    :sparkles: feat(BoardRepositoryAnnotation): 쿼리를 xml 파일에 구현하지 않고 Provider 사용

---
+++ src/main/java/io/github/yeop/board/mvc_board/repository/repositoryAnnotation/BoardRepositoryAnnotation.java
@@ -0,0 +1,28 @@
+package io.github.yeop.board.mvc_board.repository.repositoryAnnotation;
+
+import io.github.yeop.board.mvc_board.domain.Board;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
+
+import java.util.List;
+
+public interface BoardRepositoryAnnotation {
+    @SelectProvider(type = RepositoryAnnotationImpl.class, method = "findByAll")
+    List<Board> findByAll();
+
+    @InsertProvider(type = RepositoryAnnotationImpl.class, method = "insertBoard")
+    void insertBoard(Board board);
+
+    @UpdateProvider(type = RepositoryAnnotationImpl.class, method = "updateHitCount")
+    void updateHitCount(int id);
+
+    @SelectProvider(type = RepositoryAnnotationImpl.class, method = "selectBoardDetail")
+    Board selectBoardDetail(int id);
+
+    @UpdateProvider(type = RepositoryAnnotationImpl.class, method = "updateBoard")
+    void updateBoard(Board board);
+
+    @UpdateProvider(type = RepositoryAnnotationImpl.class, method = "deleteBoard")
+    void deleteBoard(int id);
+}
