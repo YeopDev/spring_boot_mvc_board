6b829c98f6c0b1f021e96aa5d79e0700a012c387
sangyeop <yeop1.dev@gmail.com>
2023-06-28 14:08:11 +0900

    :recycle: refactor(BoardServiceImpl): 구현체 수정

--- src/main/java/io/github/yeop/board/mvc_board/service/BoardServiceImpl.java
+++ src/main/java/io/github/yeop/board/mvc_board/service/BoardServiceImpl.java
@@ -1,47 +1,69 @@
 package io.github.yeop.board.mvc_board.service;
 
+import io.github.yeop.board.mvc_board.domain.Board;
 import io.github.yeop.board.mvc_board.dto.BoardDetailDto;
 import io.github.yeop.board.mvc_board.dto.BoardListDto;
 import io.github.yeop.board.mvc_board.dto.InsertBoardDto;
 import io.github.yeop.board.mvc_board.dto.UpdateBoardDto;
-import io.github.yeop.board.mvc_board.mapper.BoardMapper;
+import io.github.yeop.board.mvc_board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
-//    private final BoardRepository boardRepository;
-    private final BoardMapper boardMapper;
+    private final BoardRepository boardRepository;
 
     @Override
     @Transactional(readOnly = true)
     public List<BoardListDto> selectBoardList() {
-        return boardMapper.selectBoardList();
+        List<Board> boardList = boardRepository.selectBoardList();
+        return boardList.stream()
+                .map(board -> BoardListDto.builder()
+                        .id(board.id())
+                        .title(board.title())
+                        .contents(board.contents())
+                        .hitCnt(board.hitCnt())
+                        .createdDatetime(board.createdDatetime())
+                        .formattedCreatedDatetime(board.createdDatetime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                        .build())
+                .toList();
     }
 
     @Override
-    public void insertBoard(InsertBoardDto board) {
-        boardMapper.insertBoard(board);
+    public void insertBoard(InsertBoardDto boardDto) {
+        Board board = Board.builder()
+                .id(boardDto.id())
+                .title(boardDto.title())
+                .contents(boardDto.contents())
+                .build();
+        boardRepository.insertBoard(board);
     }
 
     @Override
     public BoardDetailDto selectBoardDetail(int id) {
-        boardMapper.updateHitCount(id);
-        return boardMapper.selectBoardDetail(id);
+        boardRepository.updateHitCount(id);
+        Board board = boardRepository.selectBoardDetail(id);
+        return BoardDetailDto.from(board);
     }
 
     @Override
-    public void updateBoard(UpdateBoardDto board) {
-        boardMapper.updateBoard(board);
+    public void updateBoard(UpdateBoardDto boardDto) {
+        Board board = Board.builder()
+                .id(boardDto.id())
+                .title(boardDto.title())
+                .contents(boardDto.contents())
+                .build();
+        boardRepository.updateBoard(board);
     }
 
     @Override
     public void deleteBoard(int id) {
-        boardMapper.deleteBoard(id);
+        boardRepository.deleteBoard(id);
     }
 }
