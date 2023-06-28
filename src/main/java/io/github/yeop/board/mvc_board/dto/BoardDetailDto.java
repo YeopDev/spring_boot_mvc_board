2e1df955d9932aa06fbc464dbb62d854a2943912
sangyeop <yeop1.dev@gmail.com>
2023-06-28 14:05:56 +0900

    :recycle: refactor(BoardDetailDto): DTO 수정

--- src/main/java/io/github/yeop/board/mvc_board/dto/BoardDetailDto.java
+++ src/main/java/io/github/yeop/board/mvc_board/dto/BoardDetailDto.java
@@ -1,16 +1,23 @@
 package io.github.yeop.board.mvc_board.dto;
 
+import io.github.yeop.board.mvc_board.domain.Board;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class BoardDetailDto{
     private final int id;
     private final String title;
     private final String contents;
     private final int hitCnt;
-    private final String createdDatetime;
+    private final LocalDateTime createdDatetime;
+    private final String formattedCreatedDatetime;
     private final String creatorId;
 
     public static BoardDetailDto from(Board board) {
