1ba31c35fa4569d70697940ab98bbb6bf9bfddb9
sangyeop <yeop1.dev@gmail.com>
2023-06-28 14:10:24 +0900

    :recycle: refactor(BoardRepositoryImpl): Dto -> entity 변경

---
+++ src/main/java/io/github/yeop/board/mvc_board/repository/BoardRepositoryImpl.java
@@ -0,0 +1,48 @@
+package io.github.yeop.board.mvc_board.repository;
+
+import io.github.yeop.board.mvc_board.domain.Board;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@MapperScan
+@Repository
+@RequiredArgsConstructor
+public class BoardRepositoryImpl implements BoardRepository {
+    private final SqlSession sqlSession;
+
+    @Override
+    public List<Board> selectBoardList() {
+        return sqlSession.selectList("selectBoardList");
+    }
+
+    @Override
+    public void insertBoard(Board board) {
+        sqlSession.insert("insertBoard", board);
+    }
+
+    @Override
+    public void updateHitCount(int id) {
+        sqlSession.update("updateHitCount", id);
+    }
+
+    @Override
+    public Board selectBoardDetail(int id) {
+        return sqlSession.selectOne("selectBoardDetail", id);
+    }
+
+    @Override
+    public void updateBoard(Board board) {
+        sqlSession.update("updateBoard", board);
+    }
+
+    @Override
+    public void deleteBoard(int id) {
+        sqlSession.update("deleteBoard", id);
+    }
+}
