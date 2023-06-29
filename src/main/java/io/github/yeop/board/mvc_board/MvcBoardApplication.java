6b415985e3a441d88423f8453e7676cd3b437d70
sangyeop <yeop1.dev@gmail.com>
2023-06-30 02:06:30 +0900

    :recycle: refactor(MvcBoardApplication): Mapper를 읽기위해 경로지정

--- src/main/java/io/github/yeop/board/mvc_board/MvcBoardApplication.java
+++ src/main/java/io/github/yeop/board/mvc_board/MvcBoardApplication.java
@@ -1,11 +1,13 @@
 package io.github.yeop.board.mvc_board;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
+@MapperScan("io.github.yeop.board.mvc_board.repository.repositoryMapper")
 public class MvcBoardApplication {
 
 	public static void main(String[] args) {
