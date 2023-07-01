510462760bb1d3a163fbd9a894d6ba2d1e72a059
sangyeop <yeop1.dev@gmail.com>
2023-07-02 03:13:56 +0900

    :recycle: refactor(InsertBoardDto): id삭제

--- src/main/java/io/github/yeop/board/mvc_board/dto/InsertBoardDto.java
+++ src/main/java/io/github/yeop/board/mvc_board/dto/InsertBoardDto.java
@@ -1,4 +1,4 @@
 package io.github.yeop.board.mvc_board.dto;
 
-public record InsertBoardDto(int id, String title, String contents) {
+public record InsertBoardDto(String title, String contents) {
 }
