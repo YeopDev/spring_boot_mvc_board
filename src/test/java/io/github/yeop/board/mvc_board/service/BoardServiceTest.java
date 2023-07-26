964986c7960c3c312338eac109bfc3c47734c8bb
sangyeop <yeop1.dev@gmail.com>
2023-07-26 10:09:26 +0900

    :white_check_mark: test(BoardServiceTest): 테스트

--- src/test/java/io/github/yeop/board/mvc_board/service/BoardServiceTest.java
+++ src/test/java/io/github/yeop/board/mvc_board/service/BoardServiceTest.java
@@ -2,7 +2,7 @@ package io.github.yeop.board.mvc_board.service;
 
 import io.github.yeop.board.mvc_board.domain.Board;
 import io.github.yeop.board.mvc_board.dto.BoardListDto;
-import io.github.yeop.board.mvc_board.repository.repositoryMapper.BoardMapperRepository;
+import io.github.yeop.board.mvc_board.dto.InsertBoardDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,23 +12,20 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardServiceTest {
-
-    @Autowired
     BoardService boardService;
-
-    @Autowired
-    BoardMapperRepository boardMapperRepository;
 
     @Test
     void selectBoardList() {
-        //given 이러이러한 일
-        Board board = new Board(1, "boardA", "content", 1, "userA", LocalDateTime.now(), "userA", LocalDateTime.now());
+        //given
+//        Board board = new Board(1, "boardA", "content", 1, "userA", LocalDateTime.now(), "userA", LocalDateTime.now());
+        boardService.insertBoard(new InsertBoardDto("boardA", "content"));
 
-        //when 이렇게 됐을때
+        //when
         List<BoardListDto> findBoardList = boardService.selectBoardList();
 
-        //then 이렇게 된다
+        //then
         assertThat(findBoardList).hasSize(1);
     }
+
 
 }
